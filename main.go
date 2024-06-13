c76e7c2cd034d2dcb2af43ed0f1ca9a222fa057f
Jack Conner <1jackconner@gmail.com>
2024-06-12 19:59:42 -0700

    implement post users endpoint

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/bigbabyjack/blog-aggregator/internal/database"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
 	port := os.Getenv("PORT")
+	// dbURL := os.Getenv("DATABASE_URL")
+	// db, err := sql.Open("postgres", dbURL)
+	// if err != nil {
+	// 	log.Fatal("Unable to open database.")
+	// }
+	// dbQueries := database.New(db)
+	//
+	// cfg := apiConfig{
+	// 	DB: dbQueries,
+	// }
 
 	mux := http.NewServeMux()
 	srv := &http.Server{
@@ -26,6 +45,39 @@ func main() {
 
 	mux.HandleFunc("GET /v1/err", func(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+	})
+
+	mux.HandleFunc("POST /v1/users", func(w http.ResponseWriter, r *http.Request) {
+		decoder := json.NewDecoder(r.Body)
+		params := struct {
+			Name string `json:"name"`
+		}{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, err.Error())
+			return
+		}
+		user := database.User{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			Name: sql.NullString{String: params.Name,
+				Valid: params.Name != ""},
+		}
+		// cfg.DB.CreateUser(user)
+		response := struct {
+			ID        string    `json:"id"`
+			CreatedAt time.Time `json:"created_at"`
+			UpdatedAt time.Time `json:"updated_at"`
+			Name      string    `json:"name"`
+		}{
+			ID:        user.ID.String(),
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			Name:      user.Name.String, // Convert the NullString to a regular string
+		}
+		respondWithJSON(w, http.StatusCreated, response)
+
 	})
 
 	log.Fatal(srv.ListenAndServe())
