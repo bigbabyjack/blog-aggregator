131037333b8d20bcac7136b6224a65ebf3b3af2f
Jack Conner <1jackconner@gmail.com>
2024-06-16 09:47:28 -0700

    add last fetched at column to feeds table

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -1,7 +1,7 @@
 -- name: CreateFeed :one
 INSERT INTO feeds (
-    id, created_at, updated_at, name, url, user_id
-) VALUES ( $1, $2, $3, $4, $5, $6 )
+    id, created_at, updated_at, name, url, user_id, last_fetched_at
+) VALUES ( $1, $2, $3, $4, $5, $6, $7)
 RETURNING *;
 
 -- name: GetFeeds :many
